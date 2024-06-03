9c0c270ae4370b57ce13f11dba60b212fb662373
DmitrySteshenko <dmitriy-steshenko@yandex.ru>
2024-06-03 22:54:42 +0300

    Refactor storage

--- internal/server/storage/storage.go
+++ internal/server/storage/storage.go
@@ -4,33 +4,20 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
-	"time"
 )
 
 type MemStorage struct {
 	storageType    string
 	mx             sync.RWMutex
-	gaugeMetrics   map[string]*gaugeMetric
-	counterMetrics map[string]*counterMetric
-}
-
-type gaugeMetric struct {
-	mx     sync.RWMutex
-	value  float64
-	vector map[int64]float64
-}
-
-type counterMetric struct {
-	mx     sync.RWMutex
-	value  int64
-	vector map[int64]int64
+	gaugeMetrics   map[string]float64
+	counterMetrics map[string]int64
 }
 
 func NewStorage() *MemStorage {
 	return &MemStorage{
 		storageType:    "struct",
-		gaugeMetrics:   make(map[string]*gaugeMetric),
-		counterMetrics: make(map[string]*counterMetric),
+		gaugeMetrics:   make(map[string]float64),
+		counterMetrics: make(map[string]int64),
 	}
 }
 
@@ -39,95 +26,39 @@ func (s *MemStorage) GetType() string {
 }
 
 func (s *MemStorage) GetGaugeMetric(name string) (value float64, err error) {
-	metric, ok := s.gaugeMetrics[name]
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	value, ok := s.gaugeMetrics[name]
 	if !ok {
 		err = fmt.Errorf("metric %s is not found", name)
-		return
 	}
-	metric.mx.RLock()
-	defer metric.mx.RUnlock()
-	value = metric.value
-
-	return
-}
-
-func (s *MemStorage) GetGaugeMetrics(name string) (value map[int64]float64, err error) {
-	metric, ok := s.gaugeMetrics[name]
-	if !ok {
-		err = fmt.Errorf("metric %s is not found", name)
-		return
-	}
-	metric.mx.RLock()
-	defer metric.mx.RUnlock()
-	value = metric.vector
 
 	return
 }
 
 func (s *MemStorage) PutGaugeMetric(name string, value float64) (err error) {
-	metric, ok := s.gaugeMetrics[name]
-	if ok {
-		metric.mx.Lock()
-		defer metric.mx.Unlock()
-		metric.value = value
-		metric.vector[time.Now().Unix()] = value
-	} else {
-		s.mx.Lock()
-		defer s.mx.Unlock()
-		s.gaugeMetrics[name] = &gaugeMetric{
-			value: value,
-			vector: map[int64]float64{
-				time.Now().Unix(): value,
-			},
-		}
-	}
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	s.gaugeMetrics[name] = value
 
 	return
 }
 
 func (s *MemStorage) GetCounterMetric(name string) (value int64, err error) {
-	metric, ok := s.counterMetrics[name]
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	value, ok := s.counterMetrics[name]
 	if !ok {
 		err = fmt.Errorf("metric %s not found", name)
-		return
 	}
-	metric.mx.RLock()
-	defer metric.mx.RUnlock()
-	value = metric.value
-
-	return
-}
-
-func (s *MemStorage) GetCounterMetrics(name string) (value map[int64]int64, err error) {
-	metric, ok := s.counterMetrics[name]
-	if !ok {
-		err = fmt.Errorf("metric %s not found", name)
-		return
-	}
-	metric.mx.RLock()
-	defer metric.mx.RUnlock()
-	value = metric.vector
 
 	return
 }
 
 func (s *MemStorage) PutCounterMetric(name string, value int64) (err error) {
-	metric, ok := s.counterMetrics[name]
-	if ok {
-		metric.mx.Lock()
-		defer metric.mx.Unlock()
-		metric.value += value
-		metric.vector[time.Now().Unix()] = value
-	} else {
-		s.mx.Lock()
-		defer s.mx.Unlock()
-		s.counterMetrics[name] = &counterMetric{
-			value: value,
-			vector: map[int64]int64{
-				time.Now().Unix(): value,
-			},
-		}
-	}
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	s.counterMetrics[name] = s.counterMetrics[name] + value
 
 	return
 }
@@ -138,13 +69,13 @@ func (s *MemStorage) GetAllMetrics() map[string]map[string]string {
 	metrics := make(map[string]map[string]string)
 	metrics["gauges"] = make(map[string]string)
 
-	for name, gauge := range s.gaugeMetrics {
-		metrics["gauges"][name] = strconv.FormatFloat(gauge.value, 'f', -1, 64)
+	for name, value := range s.gaugeMetrics {
+		metrics["gauges"][name] = strconv.FormatFloat(value, 'f', -1, 64)
 	}
 
 	metrics["counters"] = make(map[string]string)
-	for name, counter := range s.counterMetrics {
-		metrics["counters"][name] = strconv.FormatInt(counter.value, 10)
+	for name, value := range s.counterMetrics {
+		metrics["counters"][name] = strconv.FormatInt(value, 10)
 	}
 
 	return metrics
--- internal/server/storage/storage_test.go
+++ internal/server/storage/storage_test.go
@@ -16,11 +16,6 @@ func TestGetType(t *testing.T) {
 }
 
 func TestGaugeMetric(t *testing.T) {
-	now := time.Now()
-	year := now.Year()
-	month := now.Month()
-	day := now.YearDay()
-
 	tests := []struct {
 		name  string
 		value float64
@@ -37,24 +32,11 @@ func TestGaugeMetric(t *testing.T) {
 	for i := 1; i <= 5; i++ {
 		for j, test := range tests {
 			t.Run(fmt.Sprintf("Step_%d_Key_%d", i, j), func(t *testing.T) {
-				testStorage.PutGaugeMetric(test.name, test.value)
+				err := testStorage.PutGaugeMetric(test.name, test.value)
+				assert.NoError(t, err)
 				metric, err := testStorage.GetGaugeMetric(test.name)
 				assert.NoError(t, err)
 				assert.Equal(t, metric, test.value)
-
-				metrics, err := testStorage.GetGaugeMetrics(test.name)
-				assert.NoError(t, err)
-				assert.Equal(t, i, len(metrics))
-				for key, metric := range metrics {
-					keyYear := time.Unix(key, 0).Year()
-					keyMonth := time.Unix(key, 0).Month()
-					keyDay := time.Unix(key, 0).YearDay()
-
-					assert.Equal(t, metric, test.value)
-					assert.Equal(t, keyYear, year)
-					assert.Equal(t, keyMonth, month)
-					assert.Equal(t, keyDay, day)
-				}
 			})
 		}
 		time.Sleep(time.Second)
@@ -62,11 +44,6 @@ func TestGaugeMetric(t *testing.T) {
 }
 
 func TestCounterMetric(t *testing.T) {
-	now := time.Now()
-	year := now.Year()
-	month := now.Month()
-	day := now.YearDay()
-
 	tests := []struct {
 		name  string
 		value int64
@@ -83,24 +60,11 @@ func TestCounterMetric(t *testing.T) {
 	for i := 1; i <= 5; i++ {
 		for j, test := range tests {
 			t.Run(fmt.Sprintf("Step_%d_Key_%d", i, j), func(t *testing.T) {
-				testStorage.PutCounterMetric(test.name, test.value)
+				err := testStorage.PutCounterMetric(test.name, test.value)
+				assert.NoError(t, err)
 				metric, err := testStorage.GetCounterMetric(test.name)
 				assert.NoError(t, err)
 				assert.Equal(t, metric, int64(i)*test.value)
-
-				metrics, err := testStorage.GetCounterMetrics(test.name)
-				assert.NoError(t, err)
-				assert.Equal(t, i, len(metrics))
-				for key, metric := range metrics {
-					keyYear := time.Unix(key, 0).Year()
-					keyMonth := time.Unix(key, 0).Month()
-					keyDay := time.Unix(key, 0).YearDay()
-
-					assert.Equal(t, metric, test.value)
-					assert.Equal(t, keyYear, year)
-					assert.Equal(t, keyMonth, month)
-					assert.Equal(t, keyDay, day)
-				}
 			})
 		}
 		time.Sleep(time.Second)
